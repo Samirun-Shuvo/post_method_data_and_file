0d9bdfe0dc09e5744b1939b7a9d4a1949cff15d2
Md-Samirun-Shuvo <shuvo@excelitai.com>
2022-11-02 11:15:09 +0600

    use hook from and formdata to store post request to server input data and file or image successfuly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+const App = () => {
+
+  const { register, handleSubmit } = useForm();
+
+
+  const onSubmit = (data) => {
+    // console.log(data);
+    const formData = new FormData();
+
+    Object.entries(data).forEach(([key, value]) => {
+        //console.log(value);
+        if (value instanceof FileList){
+            formData.append(key, value.item(0));
+        }
+        else{
+            formData.append(key, value);
+        }
+        
+    });
+
+    fetch(`http://192.168.0.122:8000/api/client/information/store`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="m-5">
+        <span>Name :</span>
+          <input
+            className="m-2"
+            {...register("customer_id")}
+            type="text"
+            name="customer_id"
+            placeholder="customer_id"
+          />{" "}
+          <br />
+          <span>ID :</span>
+          <input
+            className="m-2"
+            {...register("customer_name")}
+            type="text"
+            name="customer_name"
+            placeholder="customer_name"
+          />{" "}
+          <br />
+          <span>Email :</span>
+          <input
+            className="m-2"
+            {...register("customer_email")}
+            type="text"
+            name="customer_email"
+            placeholder="customer_email"
+          />{" "}
+          <br />
+          <span>Customer Image :</span>
+          <input
+            className="m-2"
+            {...register("customer_image")}
+            type="file"
+            name="customer_image"
+            multiple="multiple"
+            
+          />
+          <br />
+          <span>Nominee Image :</span>
+          <input
+            className="m-2"
+            {...register("customer_nominee_image")}
+            type="file"
+            name="customer_nominee_image"
+            multiple="multiple"
+
+          />
+          <br />
+          <span>Customer Documents :</span>
+          <input
+            className="m-2"
+            {...register("customer_document")}
+            type="file"
+            name="customer_document"
+            multiple="multiple"
+
+          />
+          <div>
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
